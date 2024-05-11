0765326296c810c51886e674aec66ec13660c876
taseenjeem <taseenjeem.dev@gmail.com>
2024-05-11 13:49:23 +0600

    added daisy ui

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Be_Vietnam_Pro } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Be_Vietnam_Pro({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,24 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00aea5",
+          secondary: "#007c67",
+          accent: "#f21b00",
+          neutral: "#262721",
+          "base-100": "#ebfff5",
+          info: "#009cb7",
+          success: "#00a580",
+          warning: "#ff9f00",
+          error: "#ff3e6e",
+        },
+      },
+    ],
+  },
 };
